3431e85387d8ecfd36195a4a41f15790da69bd5d
Stuart Black <39033186+stoogs@users.noreply.github.com>
2021-09-04 20:20:22 +0100

    Updated logic

--- mess.js
+++ mess.js
@@ -3,7 +3,7 @@ let csv = fs.readFileSync('Bitstamp_BTCUSD_d-1.csv', 'utf8');
 let csvArrayDaily = csv.split(/\r?\n/).slice(2,-1);
 const currentBtcPrice = csvArrayDaily[0].split(',')[3];
 
-const months = 43
+const months = 12
 const monthlyInvestment = 100
 let totalInvestment = months * monthlyInvestment
 
@@ -15,17 +15,19 @@ let csvArrayMonthly = (csvArray) => {
 let monthlyArray = csvArrayMonthly(csvArrayDaily)
 let monthlyArrayOpenings =  monthlyArray.map( monthly =>  monthly.split(',')[3]);
 let usersOpeningsMonths = monthlyArrayOpenings.slice(0, months)
+
 let accruedBitcoin = usersOpeningsMonths.map( (price) => {
-    return (monthlyInvestment / price);
+    const dollarPerBitcoin = 1 / price;
+    return (dollarPerBitcoin * monthlyInvestment);
 })
 
 let totalAccruedBitcoin = accruedBitcoin.reduce((a,b) => a+b)
 let gross = (totalAccruedBitcoin * currentBtcPrice).toFixed(0)
 let profit = gross - totalInvestment;
 
-console.log(`If you invested for ${months} months, ${(months/12).toFixed(1)} years,  $${monthlyInvestment}`)
+console.log(`If you invested $${monthlyInvestment} for ${months} months`)
 console.log(`Invested $`, totalInvestment)
-console.log(`Profited $`, profit)
-console.log('------------------')
-console.log(`Accrued  $ ${gross} (${(profit/totalInvestment*100).toFixed(0)})%` )
-console.log('------------------')
+console.log(`Profit   $`, profit)
+console.log('---------------------------------------')
+console.log(`Total    $ ${gross} (${ (profit/totalInvestment*100).toFixed(0)}%) and ${totalAccruedBitcoin.toFixed(4)} BTC` )
+console.log('---------------------------------------')
